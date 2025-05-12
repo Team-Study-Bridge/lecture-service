7878563ba2fae30751db5f8fa92d6a0274535601
BJH <imganchi@gmail.com>
2025-05-12 18:05:04 +0900

    동시성 단일 테스트, 테스트DB 추가

--- src/main/java/com/example/lectureservice/LectureServiceApplication.java
+++ src/main/java/com/example/lectureservice/LectureServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.lectureservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.lectureservice.repository")
+@EntityScan(basePackages = "com.example.lectureservice.entity")
 public class LectureServiceApplication {
 
     public static void main(String[] args) {
