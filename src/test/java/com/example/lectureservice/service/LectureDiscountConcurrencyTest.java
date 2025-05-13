020f645eeb55e3fb3b60d27290bec3be7d1afd40
BJH <imganchi@gmail.com>
2025-05-13 15:54:39 +0900

    동시성 단일 테스트

--- src/test/java/com/example/lectureservice/service/LectureDiscountConcurrencyTest.java
+++ src/test/java/com/example/lectureservice/service/LectureDiscountConcurrencyTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class LectureDiscountConcurrencyTest {
 
-    @Autowired
+    @Autowired //
     private LectureDiscountService discountService;
 
     @Autowired
